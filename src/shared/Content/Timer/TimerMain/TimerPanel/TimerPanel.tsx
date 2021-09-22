4497379daaec9855cd3354e18ce587d0ad2d4c4d
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2021-09-23 00:56:50 +0300

    timer

--- src/shared/Content/Timer/TimerMain/TimerPanel/TimerPanel.tsx
+++ src/shared/Content/Timer/TimerMain/TimerPanel/TimerPanel.tsx
@@ -6,15 +6,41 @@ interface ITimerPanelProps{
   className?: string;
 }
 
+const padTime = (time: number) => {
+  return String(time).length === 1 ? `0${time}` : `${time}`;
+};
+
+const getTime = (time: number, type: 'min'|'sec') => {
+  if (type === 'min') return padTime(Math.floor(time / 60));
+  if (type === 'sec') return  padTime(time % 60);
+};
+
 export function TimerPanel({className=''}: ITimerPanelProps) {
+  const [counter, setCounter] = React.useState(1500);
+
+  React.useEffect(() => {
+    let timer: any;
+    if (counter > 0) {
+      timer = setTimeout(() => setCounter(c => c - 1), 1000);
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [counter]);
+
   return (
     <div className={`${className} ${styles.timerPanel}`}>
       <div className={styles.timer}>
-        <span className={styles.time}>25:00</span>
-        <button className={styles.plus}>
-          <PlusIcon className={styles.plusIcon}/>
-        </button>
+        <span className={styles.time}>{getTime(counter, 'min')}</span> 
+        <span className={styles.time2}>:</span>
+        <span className={styles.time}>{getTime(counter, 'sec')}</span>
       </div>
+      <button className={styles.plus}>
+        <PlusIcon className={styles.plusIcon}/>
+      </button>
       <span className={styles.taskLabel}>Задача 1 - 
         <span className={styles.task}>Сверстать сайт</span> 
       </span>
